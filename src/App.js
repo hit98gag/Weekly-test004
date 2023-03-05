f7cd1b7f319bd1d60f951f98dffd7cbd6c8639ea
Hitesh Tiwari <72449162+hit98gag@users.noreply.github.com>
2023-03-05 12:35:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Buttons from './Component/Buttons';
+import Display from './Component/Display';
+import { useState,useEffect } from 'react';
 function App() {
+  const [input,setInput]=useState("")
+  const [display,setdisplay]=useState("")
+  function Calc(e){
+    setInput(input+e);
+  }
+  function getResult(){
+    setdisplay(eval(input))
+  }
+  function resetsfunc(){
+    setdisplay("")
+    setInput("")
+  }
+  useEffect(()=>{
+    setdisplay(input)
+
+  },[input])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+      <Display display={display} />
+      <Buttons Calc={Calc} getResult={getResult} resetsfunc={resetsfunc}/>
+      </div>
     </div>
   );
 }
